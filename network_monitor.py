4a2d976c34feca2b8414b4488e82e959736d25be
Dawsonkw <dawsonkw@gmail.com>
2024-05-25 20:28:24 -0600

    changed the display of the returned data to be more readable by adding in Tabulate library. reformatted with autopep8

--- network_monitor.py
+++ network_monitor.py
@@ -1,34 +1,28 @@
-import psutil
-import scapy.all 
-from scapy.all import ARP, Ether, srp
+import sys
+from collections import deque
+from scapy.all import ARP, Ether, srp, conf
 import time
 import threading
 import signal
-import sys
-import threading
-import time
-from collections import deque
-
+import psutil
+from tabulate import tabulate
 
 # Global variables for storing network traffic data
-baseline = 0 # 1 MB/s
+baseline = 0  # 1 MB/s
 
-# 
-# Functioms for network scanning and monitoring
-# 
-# Signal handler function to stop the monitoring and scanning threads when a KeyboardInterrupt is received
+# Functions for network scanning and monitoring
+
+
 def signal_handler(signal, frame):
     stop_event.set()
     print("\nMonitoring and scanning stopped.")
     sys.exit(0)
-    
-# Alert function printed in the console in a different color
-#The \033[91m and \033[0m escape sequences are used to set and reset the text color in the terminal, respectively. 
-# In this case, the message will be displayed in red color.
+
+
 def display_alert(message):
     print(f"\033[91m" + message + "\033[0m")
 
-# Network statistics
+
 def get_network_stats():
     net_io = psutil.net_io_counters()
     return {
@@ -38,45 +32,58 @@ def get_network_stats():
         'packets_recv': net_io.packets_recv,
     }
 
-# ARP scan function
+
+def convert_bytes(num):
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return f"{num:.2f} {unit}"
+        num /= 1024.0
+
+
 def arp_scan(network):
+    conf.L3socket6 = True
     arp = ARP(pdst=network)
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
     packet = ether/arp
-
     result = srp(packet, timeout=3, verbose=0)[0]
 
     devices = []
     for sent, received in result:
         devices.append({'ip': received.psrc, 'mac': received.hwsrc})
-    
+
     return devices
 
-#  Monitor and scanning functions w/ control
-def monitor_network(interface, stopEvent):
+
+def monitor_network(interface, stop_event):
     global baseline
-    traffic_threshold = 500                     #5 times the baseline)
-    
-    while not stopEvent.is_set():
+    traffic_threshold = 500  # 5 times the baseline
+
+    while not stop_event.is_set():
         try:
             stats = get_network_stats()
-            print(f"Bytes sent: {stats['bytes_sent']}, Bytes received: {stats['bytes_recv']}")
-            print(f"Packets sent: {stats['packets_sent']}, Packets received: {stats['packets_recv']}")
-            print(f"Average baseline: {(baseline * 8) / 1000000:.2f} Mbps")
-            
+            table = [
+                ["Bytes Sent", convert_bytes(stats['bytes_sent'])],
+                ["Bytes Received", convert_bytes(stats['bytes_recv'])],
+                ["Packets Sent", stats['packets_sent']],
+                ["Packets Received", stats['packets_recv']],
+                ["Average Baseline", f"{(baseline * 8) / 1000000:.2f} Mbps"]
+            ]
+            print(tabulate(table, headers=[
+                  "Metric", "Value"], tablefmt="pretty"))
+
             if baseline > 0 and stats["bytes_recv"] > baseline * traffic_threshold:
-                alert_message = f"High network traffic detected on interface {interface}! Traffic Rate: {(stats['bytes_recv'] * 8) / 1000000:.2f} Mbps, Threshold: {(baseline * traffic_threshold * 8 ) / 1000000:.2f} Mbps"
+                alert_message = f"High network traffic detected on interface {interface}! Traffic Rate: {(stats['bytes_recv'] * 8) / 1000000:.2f} Mbps, Threshold: {(baseline * traffic_threshold * 8) / 1000000:.2f} Mbps"
                 display_alert(alert_message)
-                
-            
+
             time.sleep(5)
         except KeyboardInterrupt:
             stop_event.set()
             print("\nMonitoring and scanning stopped.")
             break
 
-def scan_network(network, stopEvent):
-    while not stopEvent.is_set():
+
+def scan_network(network, stop_event):
+    while not stop_event.is_set():
         try:
             devices = arp_scan(network)
             print("Devices on network:")
@@ -87,24 +94,19 @@ def scan_network(network, stopEvent):
             stop_event.set()
             print("\nMonitoring and scanning stopped.")
             break
-        
-# baseline function to monitor the network traffic for a baseline
-def monitor_baseline(interface, stopEvent):
+
+
+def monitor_baseline(interface, stop_event):
     global baseline
-    traffic_data = deque(maxlen=60) 
-    
-    while not stopEvent.is_set():
+    traffic_data = deque(maxlen=60)
+    while not stop_event.is_set():
         try:
             stats = get_network_stats()
             bytes_recv = stats['bytes_recv']
             traffic_data.append(bytes_recv)
-            
             if len(traffic_data) == traffic_data.maxlen:
                 baseline = sum(traffic_data) // len(traffic_data) // 1000000
-                
-                
-                
-            time.sleep(1) # check every second
+            time.sleep(1)  # check every second
         except KeyboardInterrupt:
             stop_event.set()
             print("\nMonitoring and scanning stopped.")
@@ -112,30 +114,37 @@ def monitor_baseline(interface, stopEvent):
 
 
 if __name__ == "__main__":
-    interface = "eth0"
-    network = "192.168.1.0/24"
+    INTERFACE = "eth0"
+    NETWORK = "192.168.1.0/24"
     stop_event = threading.Event()
-    #Register signal handler for KeyboardInterrupt
+
+    # Register signal handler for KeyboardInterrupt
     signal.signal(signal.SIGINT, signal_handler)
+
     # Start monitoring and scanning threads
-    monitor_thread = threading.Thread(target=monitor_network, args=(interface, stop_event))
-    scan_thread = threading.Thread(target=scan_network, args=(network, stop_event))
-    baseline_thread = threading.Thread(target=monitor_baseline, args=(interface, stop_event))
-    
+    monitor_thread = threading.Thread(
+        target=monitor_network, args=(INTERFACE, stop_event))
+    scan_thread = threading.Thread(
+        target=scan_network, args=(NETWORK, stop_event))
+    baseline_thread = threading.Thread(
+        target=monitor_baseline, args=(INTERFACE, stop_event))
+
     baseline_thread.start()
     monitor_thread.start()
     scan_thread.start()
-    
-    
-    # loops to control the monitoring and scanning processes
+
+    # Loop to control the monitoring and scanning processes
     while True:
         try:
-            command = input("Enter 'start' to start monitoring and scanning, 'stop' to stop, or 'exit' to exit: ").strip().lower()
+            command = input(
+                "Enter 'start' to start monitoring and scanning, 'stop' to stop, or 'exit' to exit: ").strip().lower()
             if command == "start":
-                if not monitor_thread.is_alive() and not scan_thread.is_alive():
+                if not any(thread.is_alive() for thread in [monitor_thread, scan_thread]):
                     stop_event.clear()
-                    monitor_thread = threading.Thread(target=monitor_network, args=(interface, stop_event))
-                    scan_thread = threading.Thread(target=scan_network, args=(network, stop_event))
+                    monitor_thread = threading.Thread(
+                        target=monitor_network, args=(INTERFACE, stop_event))
+                    scan_thread = threading.Thread(
+                        target=scan_network, args=(NETWORK, stop_event))
                     monitor_thread.start()
                     scan_thread.start()
                     print("Monitoring and scanning started.")
@@ -145,7 +154,7 @@ if __name__ == "__main__":
                 stop_event.set()
                 monitor_thread.join()
                 scan_thread.join()
-                print("Monitoring and scanning stopped.")   
+                print("Monitoring and scanning stopped.")
             elif command == "exit":
                 stop_event.set()
                 monitor_thread.join()
@@ -154,12 +163,11 @@ if __name__ == "__main__":
                 break
             else:
                 print("Invalid command. Please enter 'start', 'stop', or 'exit'.")
-                
+
         except KeyboardInterrupt:
             stop_event.set()
             baseline_thread.join()
             monitor_thread.join()
             scan_thread.join()
             print("\nExiting program.")
-            break   
-
+            break
